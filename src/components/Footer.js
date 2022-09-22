f44cfd606ed39a74073df5a351fe06985d9ed6a5
Michael <mikelhefzy@gmail.com>
2022-09-22 13:00:32 +0200

    add social link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ function Footer() {
     return(
     <footer>
         <div className="icons">
-        <a href="https://www.figma.com" className="icon"><FaFacebookSquare /></a>
-        <a href="https://www.figma.com" className="icon"><AiFillTwitterSquare /></a>
-        <a href="https://www.figma.com" className="icon"><FaInstagramSquare /></a>
-        <a href="https://www.figma.com" className="icon"><FaGithubSquare /></a>
-        <a href="https://www.figma.com" className="icon"><FaLinkedin /></a>
+        <a href="https://www.facebook.com/profile.php?id=100023482831143" className="icon"><FaFacebookSquare /></a>
+        <a href="https://twitter.com/mikelhfzy" className="icon"><AiFillTwitterSquare /></a>
+        <a href="https://www.instagram.com/michael_anwar22/?hl=en" className="icon"><FaInstagramSquare /></a>
+        <a href="https://github.com/mikget66" className="icon"><FaGithubSquare /></a>
+        <a href="#" className="icon"><FaLinkedin /></a>
         </div>
     </footer>
     );
